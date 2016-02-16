468ce50a34417a9deb4f5459cd609a38c6298256
Damián Prieto <damianprietolopez@gmail.com>
2016-02-16 19:20:49 +0100

    Cambios en main controller. Revisar/preguntar componentes Figura 5. Vista de VoterInfo

--- src/main/java/hello/MainController.java
+++ src/main/java/hello/MainController.java
@@ -1,15 +1,16 @@
 package hello;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 
-    @RequestMapping("/user")
-    public UserInfo user() {
-    	
-        return null;
+    @RequestMapping(value="/user", method = RequestMethod.POST)
+    public String user() {
+    	UserInfo usuario = GetVoterInfo.find("pepe","1234");
+        return " ";
 
     }
 
